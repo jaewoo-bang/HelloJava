6753c5aa36b5dd7a2188c1f27f12a080646830dc
jaewoo bang <jaewoo@gamil.com>
2025-07-21 15:12:57 +0900

    jdbc 시작

---
+++ jdbcProject/src/com/yedam/app/DButill.java
@@ -0,0 +1,21 @@
+package com.yedam.app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+// ojdbc11.jar 외부라이브러리를 추가.
+public class DButill {
+	// DB접속정보 활용 -> 세션(Connection객체)
+	public static Connection getConnect() {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","scott" ,"tiger");
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+		
+	}
+}
